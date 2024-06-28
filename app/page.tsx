58b1ee0ed455f75ac0d6052eeb52a10b7feaaaa1
oliveross123 <oliverstroch@seznam.cz>
2024-06-28 18:15:46 +0200

    Předěláni ikonek

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next"
-import { useState } from "react"
 import { Button } from "components/Button/Button"
 import { LP_GRID_ITEMS } from "lp-items"
 
@@ -27,14 +26,14 @@ export default function Web() {
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              🚀 Objevte budoucnost podnikové automatizace s <span className="text-orange-500">Swiftly! </span>{" "}
+              🚀 Objevte budoucnost podnikové automatizace s <span className="text-orange-500">Swiftly! </span>
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
               Zjednodušte své každodenní úkoly a zvyšte produktivitu s naším intuitivním řešením. Připojte se k nám na
-              začátku naší cesty a buďte mezi prvními, kdo vyzkouší Swiftly!{" "}
+              začátku naší cesty a buďte mezi prvními, kdo vyzkouší Swiftly!
             </p>
             <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
-              Early Acces
+              Early Access
             </Button>
             <Button
               href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
@@ -50,7 +49,12 @@ export default function Web() {
           <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
             {LP_GRID_ITEMS.map((singleItem) => (
               <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
-                <div className="text-black-700 mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-400 p-1.5 dark:bg-primary-200 lg:h-12 lg:w-12">
+                {/* Přidání fallbacků pro ikony */}
+                <div
+                  role="img" // Použijeme role "img" pro větší kompatibilitu
+                  className="text-black-700 mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-400 p-1.5 dark:bg-primary-200 lg:h-12 lg:w-12"
+                  style={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+                >
                   {singleItem.icon}
                 </div>
                 <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
