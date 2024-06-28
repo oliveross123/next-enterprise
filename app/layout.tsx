5b2b66a352de3db159c2cad93733974b9f740eb5
oliveross123 <oliverstroch@seznam.cz>
2024-06-28 17:28:26 +0200

    přidání scriptu pro mailchimp v layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Swiftly.cz | Software pro automatizaci processů pro střední, male a mirko podnikatelé</title>
+        <title>Swiftly.cz | Software pro automatizaci procesů pro střední, malé a mikro podnikatele</title>
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="og:url" content="https://next-enterprise.vercel.app/" />
         <meta
           property="og:image"
           content="https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png"
+        />
+        {/* Přidání vlastního skriptu */}
+        <script
+          id="mcjs"
+          dangerouslySetInnerHTML={{
+            __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/ed718c28d97fab0e745e57174/dc01543b5e28a42bc4ebf806f.js");`,
+          }}
         />
       </head>
       <body>{children}</body>
